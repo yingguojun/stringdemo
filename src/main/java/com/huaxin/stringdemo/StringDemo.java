a934c2ba18e3bba9fd7f8512fe55e75d02b4a1e4
yingguojun <yingguojun@163.com>
2017-12-24 20:19:08 +0800

    小乌龟修改测试

--- src/main/java/com/huaxin/stringdemo/StringDemo.java
+++ src/main/java/com/huaxin/stringdemo/StringDemo.java
@@ -9,6 +9,7 @@ public class StringDemo {
 		String a1 = "A";
 	    String a2 =  new String();
 	    String a3 =  new String("A");
+	    String a4 =  new String("A");
 	    
 	    char value[] = new char[10];
 	    String a4 =  new String(value);
